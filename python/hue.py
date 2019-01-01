29869b838d7154a6e6005625ba2a561c3348b6e4
Gabe Fierro <gtfierro225@gmail.com>
2018-12-31 18:12:06 -0800

    hue driver takes config

--- python/hue.py
+++ python/hue.py
@@ -1,16 +1,28 @@
 from pyxbos import *
+import logging
 import time
 from phue import Bridge
 
 
 class Example(Driver):
-    def setup(self):
+    def setup(self, cfg):
         print("setting up")
 
-        self.b = Bridge('192.168.1.84')
+        self.b = Bridge(cfg['hue_bridge'])
         self.b.connect()
         self.b.get_api()
+        self.lights = {}
+        for l in self.b.lights:
+            self.lights[l.name.replace(' ','_')] = l
+        print(self.b.lights)
 
+    def write(self, uri, age, msg):
+        print('write',uri, age)
+        if msg.XBOSIoTDeviceActuation is not None:
+            l = msg.XBOSIoTDeviceActuation.light
+            name = uri.split('/')[-1]
+            self.lights[name].on = l.state
+            self.lights[name].brightness = l.brightness
 
 
     def read(self):
