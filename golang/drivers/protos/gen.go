06f606acee7d31ad3d794bf8c124a59180df4825
John Yang <john.yang20@berkeley.edu>
2019-05-20 17:31:12 -0700

    Removing golang changes (accidentally merged another branch into this one

--- golang/drivers/protos/gen.go
+++
@@ -1,5 +0,0 @@
-package mqpb
-
-//ignore go:generate protoc -I/usr/local/include -I. -I$GOPATH/src/github.com/immesys/wave/eapi/pb -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis --go_out=plugins=grpc:. wavemq.proto
-
-//go:generate protoc -I/usr/local/include -I. -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis --go_out=plugins=grpc:. wavemq.proto eapi.proto
