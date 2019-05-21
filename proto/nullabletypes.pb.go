d7e0ee6d9873f2fef007efd8e4668b12c15fcb75
Gabe Fierro <gtfierro@cs.berkeley.edu>
2019-05-20 22:09:02 -0700

    add compiled protobuf files

--- proto/nullabletypes.pb.go
+++ proto/nullabletypes.pb.go
@@ -20,6 +20,99 @@ var _ = math.Inf
 // proto package needs to be updated.
 const _ = proto.ProtoPackageIsVersion3 // please upgrade the proto package
 
+type FanModeValue int32
+
+const (
+	FanModeValue_FanAuto FanModeValue = 0
+	FanModeValue_FanOn   FanModeValue = 1
+	FanModeValue_FanOff  FanModeValue = 2
+)
+
+var FanModeValue_name = map[int32]string{
+	0: "FanAuto",
+	1: "FanOn",
+	2: "FanOff",
+}
+
+var FanModeValue_value = map[string]int32{
+	"FanAuto": 0,
+	"FanOn":   1,
+	"FanOff":  2,
+}
+
+func (x FanModeValue) String() string {
+	return proto.EnumName(FanModeValue_name, int32(x))
+}
+
+func (FanModeValue) EnumDescriptor() ([]byte, []int) {
+	return fileDescriptor_a606d93d67bf0130, []int{0}
+}
+
+type HVACModeValue int32
+
+const (
+	HVACModeValue_HVACModeOff      HVACModeValue = 0
+	HVACModeValue_HVACModeHeatOnly HVACModeValue = 1
+	HVACModeValue_HVACModeCoolOnly HVACModeValue = 2
+	HVACModeValue_HVACModeAuto     HVACModeValue = 3
+)
+
+var HVACModeValue_name = map[int32]string{
+	0: "HVACModeOff",
+	1: "HVACModeHeatOnly",
+	2: "HVACModeCoolOnly",
+	3: "HVACModeAuto",
+}
+
+var HVACModeValue_value = map[string]int32{
+	"HVACModeOff":      0,
+	"HVACModeHeatOnly": 1,
+	"HVACModeCoolOnly": 2,
+	"HVACModeAuto":     3,
+}
+
+func (x HVACModeValue) String() string {
+	return proto.EnumName(HVACModeValue_name, int32(x))
+}
+
+func (HVACModeValue) EnumDescriptor() ([]byte, []int) {
+	return fileDescriptor_a606d93d67bf0130, []int{1}
+}
+
+type HVACStateValue int32
+
+const (
+	HVACStateValue_HVACStateOff        HVACStateValue = 0
+	HVACStateValue_HVACStateHeatStage1 HVACStateValue = 1
+	HVACStateValue_HVACStateCoolStage1 HVACStateValue = 2
+	HVACStateValue_HVACStateHeatStage2 HVACStateValue = 3
+	HVACStateValue_HVACStateCoolStage2 HVACStateValue = 4
+)
+
+var HVACStateValue_name = map[int32]string{
+	0: "HVACStateOff",
+	1: "HVACStateHeatStage1",
+	2: "HVACStateCoolStage1",
+	3: "HVACStateHeatStage2",
+	4: "HVACStateCoolStage2",
+}
+
+var HVACStateValue_value = map[string]int32{
+	"HVACStateOff":        0,
+	"HVACStateHeatStage1": 1,
+	"HVACStateCoolStage1": 2,
+	"HVACStateHeatStage2": 3,
+	"HVACStateCoolStage2": 4,
+}
+
+func (x HVACStateValue) String() string {
+	return proto.EnumName(HVACStateValue_name, int32(x))
+}
+
+func (HVACStateValue) EnumDescriptor() ([]byte, []int) {
+	return fileDescriptor_a606d93d67bf0130, []int{2}
+}
+
 type Int32 struct {
 	Value                int32    `protobuf:"varint,1,opt,name=value,proto3" json:"value,omitempty"`
 	XXX_NoUnkeyedLiteral struct{} `json:"-"`
@@ -215,24 +308,159 @@ func (m *Bool) GetValue() bool {
 	return false
 }
 
+type FanMode struct {
+	Value                FanModeValue `protobuf:"varint,1,opt,name=value,proto3,enum=xbospb.FanModeValue" json:"value,omitempty"`
+	XXX_NoUnkeyedLiteral struct{}     `json:"-"`
+	XXX_unrecognized     []byte       `json:"-"`
+	XXX_sizecache        int32        `json:"-"`
+}
+
+func (m *FanMode) Reset()         { *m = FanMode{} }
+func (m *FanMode) String() string { return proto.CompactTextString(m) }
+func (*FanMode) ProtoMessage()    {}
+func (*FanMode) Descriptor() ([]byte, []int) {
+	return fileDescriptor_a606d93d67bf0130, []int{5}
+}
+
+func (m *FanMode) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_FanMode.Unmarshal(m, b)
+}
+func (m *FanMode) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_FanMode.Marshal(b, m, deterministic)
+}
+func (m *FanMode) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_FanMode.Merge(m, src)
+}
+func (m *FanMode) XXX_Size() int {
+	return xxx_messageInfo_FanMode.Size(m)
+}
+func (m *FanMode) XXX_DiscardUnknown() {
+	xxx_messageInfo_FanMode.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_FanMode proto.InternalMessageInfo
+
+func (m *FanMode) GetValue() FanModeValue {
+	if m != nil {
+		return m.Value
+	}
+	return FanModeValue_FanAuto
+}
+
+type HVACMode struct {
+	Value                HVACModeValue `protobuf:"varint,1,opt,name=value,proto3,enum=xbospb.HVACModeValue" json:"value,omitempty"`
+	XXX_NoUnkeyedLiteral struct{}      `json:"-"`
+	XXX_unrecognized     []byte        `json:"-"`
+	XXX_sizecache        int32         `json:"-"`
+}
+
+func (m *HVACMode) Reset()         { *m = HVACMode{} }
+func (m *HVACMode) String() string { return proto.CompactTextString(m) }
+func (*HVACMode) ProtoMessage()    {}
+func (*HVACMode) Descriptor() ([]byte, []int) {
+	return fileDescriptor_a606d93d67bf0130, []int{6}
+}
+
+func (m *HVACMode) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_HVACMode.Unmarshal(m, b)
+}
+func (m *HVACMode) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_HVACMode.Marshal(b, m, deterministic)
+}
+func (m *HVACMode) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_HVACMode.Merge(m, src)
+}
+func (m *HVACMode) XXX_Size() int {
+	return xxx_messageInfo_HVACMode.Size(m)
+}
+func (m *HVACMode) XXX_DiscardUnknown() {
+	xxx_messageInfo_HVACMode.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_HVACMode proto.InternalMessageInfo
+
+func (m *HVACMode) GetValue() HVACModeValue {
+	if m != nil {
+		return m.Value
+	}
+	return HVACModeValue_HVACModeOff
+}
+
+type HVACState struct {
+	Value                HVACStateValue `protobuf:"varint,1,opt,name=value,proto3,enum=xbospb.HVACStateValue" json:"value,omitempty"`
+	XXX_NoUnkeyedLiteral struct{}       `json:"-"`
+	XXX_unrecognized     []byte         `json:"-"`
+	XXX_sizecache        int32          `json:"-"`
+}
+
+func (m *HVACState) Reset()         { *m = HVACState{} }
+func (m *HVACState) String() string { return proto.CompactTextString(m) }
+func (*HVACState) ProtoMessage()    {}
+func (*HVACState) Descriptor() ([]byte, []int) {
+	return fileDescriptor_a606d93d67bf0130, []int{7}
+}
+
+func (m *HVACState) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_HVACState.Unmarshal(m, b)
+}
+func (m *HVACState) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_HVACState.Marshal(b, m, deterministic)
+}
+func (m *HVACState) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_HVACState.Merge(m, src)
+}
+func (m *HVACState) XXX_Size() int {
+	return xxx_messageInfo_HVACState.Size(m)
+}
+func (m *HVACState) XXX_DiscardUnknown() {
+	xxx_messageInfo_HVACState.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_HVACState proto.InternalMessageInfo
+
+func (m *HVACState) GetValue() HVACStateValue {
+	if m != nil {
+		return m.Value
+	}
+	return HVACStateValue_HVACStateOff
+}
+
 func init() {
+	proto.RegisterEnum("xbospb.FanModeValue", FanModeValue_name, FanModeValue_value)
+	proto.RegisterEnum("xbospb.HVACModeValue", HVACModeValue_name, HVACModeValue_value)
+	proto.RegisterEnum("xbospb.HVACStateValue", HVACStateValue_name, HVACStateValue_value)
 	proto.RegisterType((*Int32)(nil), "xbospb.Int32")
 	proto.RegisterType((*Int64)(nil), "xbospb.Int64")
 	proto.RegisterType((*Uint64)(nil), "xbospb.Uint64")
 	proto.RegisterType((*Double)(nil), "xbospb.Double")
 	proto.RegisterType((*Bool)(nil), "xbospb.Bool")
+	proto.RegisterType((*FanMode)(nil), "xbospb.FanMode")
+	proto.RegisterType((*HVACMode)(nil), "xbospb.HVACMode")
+	proto.RegisterType((*HVACState)(nil), "xbospb.HVACState")
 }
 
 func init() { proto.RegisterFile("nullabletypes.proto", fileDescriptor_a606d93d67bf0130) }
 
 var fileDescriptor_a606d93d67bf0130 = []byte{
-	// 127 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xe2, 0x12, 0xce, 0x2b, 0xcd, 0xc9,
-	0x49, 0x4c, 0xca, 0x49, 0x2d, 0xa9, 0x2c, 0x48, 0x2d, 0xd6, 0x2b, 0x28, 0xca, 0x2f, 0xc9, 0x17,
-	0x62, 0xab, 0x48, 0xca, 0x2f, 0x2e, 0x48, 0x52, 0x92, 0xe5, 0x62, 0xf5, 0xcc, 0x2b, 0x31, 0x36,
-	0x12, 0x12, 0xe1, 0x62, 0x2d, 0x4b, 0xcc, 0x29, 0x4d, 0x95, 0x60, 0x54, 0x60, 0xd4, 0x60, 0x0d,
-	0x82, 0x70, 0xa0, 0xd2, 0x66, 0x26, 0xa8, 0xd2, 0xcc, 0x30, 0x69, 0x39, 0x2e, 0xb6, 0xd0, 0x4c,
-	0x4c, 0x79, 0x16, 0x24, 0x79, 0x97, 0xfc, 0xd2, 0xa4, 0x9c, 0x54, 0x54, 0x79, 0x46, 0x98, 0xbc,
-	0x0c, 0x17, 0x8b, 0x53, 0x7e, 0x7e, 0x0e, 0xaa, 0x2c, 0x07, 0x54, 0x36, 0x89, 0x0d, 0xec, 0x54,
-	0x63, 0x40, 0x00, 0x00, 0x00, 0xff, 0xff, 0x76, 0x2e, 0x5c, 0xff, 0xc1, 0x00, 0x00, 0x00,
+	// 312 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x6c, 0x92, 0x4d, 0x4b, 0xc3, 0x30,
+	0x1c, 0xc6, 0x97, 0xad, 0xad, 0xdb, 0x7f, 0x73, 0x86, 0xac, 0xbe, 0x1c, 0x54, 0xc4, 0x93, 0x54,
+	0x29, 0xd8, 0xf9, 0x82, 0xc7, 0x39, 0x19, 0xf3, 0x20, 0x03, 0x87, 0x3b, 0x0a, 0x29, 0x66, 0x22,
+	0x84, 0xa4, 0xb8, 0x54, 0xdc, 0x17, 0xf0, 0x73, 0x4b, 0xd2, 0x46, 0x12, 0xdc, 0xad, 0x7d, 0x7e,
+	0xcf, 0x8f, 0xff, 0x73, 0x08, 0x0c, 0x44, 0xc9, 0x39, 0xcd, 0x39, 0x53, 0xeb, 0x82, 0xad, 0xd2,
+	0xe2, 0x53, 0x2a, 0x49, 0xa2, 0xef, 0x5c, 0xae, 0x8a, 0xfc, 0xf4, 0x08, 0xc2, 0x47, 0xa1, 0x86,
+	0x19, 0x89, 0x21, 0xfc, 0xa2, 0xbc, 0x64, 0x07, 0xe8, 0x04, 0x9d, 0x85, 0xcf, 0xd5, 0x4f, 0x8d,
+	0x6f, 0xae, 0x7c, 0xdc, 0xb2, 0xf8, 0x18, 0xa2, 0x97, 0x8f, 0xff, 0x3c, 0x70, 0xf8, 0x83, 0x2c,
+	0x73, 0xce, 0x7c, 0x8e, 0x2c, 0x3f, 0x84, 0xe0, 0x5e, 0x4a, 0xee, 0xd3, 0xb6, 0xa5, 0xd7, 0xb0,
+	0x35, 0xa1, 0xe2, 0x49, 0xbe, 0x31, 0x92, 0xb8, 0x85, 0x7e, 0x16, 0xa7, 0xd5, 0xfc, 0xb4, 0xe6,
+	0x0b, 0xcd, 0xac, 0x76, 0x0b, 0xed, 0xe9, 0x62, 0x34, 0x36, 0xde, 0xb9, 0xef, 0xed, 0x5a, 0xcf,
+	0x16, 0x3c, 0xf1, 0x0e, 0x3a, 0x3a, 0x9f, 0x2b, 0xaa, 0x18, 0xb9, 0xf0, 0xcd, 0x3d, 0xd7, 0x34,
+	0x0d, 0x57, 0x4d, 0x32, 0xe8, 0xb9, 0x53, 0x48, 0xd7, 0x4c, 0x1f, 0x95, 0x4a, 0xe2, 0x06, 0xe9,
+	0x40, 0x38, 0xa1, 0x62, 0x26, 0x30, 0x22, 0x00, 0x91, 0xfe, 0x5c, 0x2e, 0x71, 0x33, 0x79, 0x85,
+	0x6d, 0x6f, 0x06, 0xd9, 0x81, 0xae, 0x0d, 0x74, 0xa3, 0x41, 0x62, 0xc0, 0x36, 0x98, 0x32, 0xaa,
+	0x66, 0x82, 0xaf, 0x31, 0x72, 0xd3, 0xb1, 0x94, 0xdc, 0xa4, 0x4d, 0x82, 0xa1, 0x67, 0x53, 0x73,
+	0xb6, 0x95, 0xfc, 0x20, 0xe8, 0xfb, 0x6b, 0x6d, 0xc9, 0x24, 0xd5, 0x89, 0x7d, 0x18, 0xfc, 0x25,
+	0xfa, 0xc6, 0x5c, 0xd1, 0x77, 0x76, 0x89, 0x91, 0x07, 0xf4, 0x99, 0x1a, 0x34, 0x37, 0x1b, 0x19,
+	0x6e, 0x6d, 0x36, 0x32, 0x1c, 0xe4, 0x91, 0x79, 0x72, 0xc3, 0xdf, 0x00, 0x00, 0x00, 0xff, 0xff,
+	0x11, 0xad, 0xc6, 0x31, 0x89, 0x02, 0x00, 0x00,
 }
