fd4daa4d80c534e4f4c082c9c058227cc5f47bc1
Gabe Fierro <gtfierro225@gmail.com>
2019-05-26 15:27:12 -0700

    using new AddReport method

--- driver/virtual_thermostat/virtual_thermostat.go
+++ driver/virtual_thermostat/virtual_thermostat.go
@@ -38,70 +38,71 @@ func newVirtualThermostatDriver(ntstats int, cfg driver.Config) *VirtualThermost
 func (driver *VirtualThermostatDriver) start() {
 	for tstatnum := 0; tstatnum < driver.ntstats; tstatnum++ {
 		tstatnum := tstatnum
-		go func() {
-			instance := fmt.Sprintf("vtstat%d", tstatnum)
-			err := driver.AddActuationCallback("virtual_thermostat", instance, func(msg *xbospb.XBOSIoTDeviceActuation, received time.Time) error {
-				tstat := msg.Thermostat
-				fmt.Println("actuation?", tstat, received)
-				if tstat.HeatingSetpoint != nil {
-					driver.hsp = tstat.HeatingSetpoint.Value
-				}
-				if tstat.CoolingSetpoint != nil {
-					driver.csp = tstat.CoolingSetpoint.Value
-				}
-				reading := &xbospb.XBOSIoTDeviceState{
-					Thermostat: &xbospb.Thermostat{
-						Temperature:     &xbospb.Double{Value: driver.temp},
-						HeatingSetpoint: &xbospb.Double{Value: driver.hsp},
-						CoolingSetpoint: &xbospb.Double{Value: driver.csp},
-						State:           &xbospb.HVACState{Value: xbospb.HVACStateValue(driver.state)},
-					},
-				}
-				fmt.Println(driver.Respond("virtual_thermostat", instance, uint64(msg.Requestid), reading))
-				return nil
-			})
-			if err != nil {
-				log.Fatal(err)
-			}
-			for {
-				// update thermostat state
-				if driver.state == 2 {
-					driver.temp -= rand.Float64()
-				} else if driver.state == 1 {
-					driver.temp += rand.Float64()
-				} else {
-					driver.temp += 0.5 - rand.Float64()
-				}
 
-				if driver.temp <= driver.hsp-1 {
-					driver.state = 1
-				} else if driver.temp >= driver.csp+1 {
-					driver.state = 2
-				} else {
-					driver.state = 0
-				}
-
-				reading := &xbospb.XBOSIoTDeviceState{
-					Thermostat: &xbospb.Thermostat{
-						Temperature:     &xbospb.Double{Value: driver.temp},
-						HeatingSetpoint: &xbospb.Double{Value: driver.hsp},
-						CoolingSetpoint: &xbospb.Double{Value: driver.csp},
-						State:           &xbospb.HVACState{Value: xbospb.HVACStateValue(driver.state)},
-					},
-				}
-				log.Println("Report?", reading, driver.Report("virtual_thermostat", instance, reading))
-				//driver.ReportContext(instance)
-				time.Sleep(2 * time.Second)
+		instance := fmt.Sprintf("vtstat%d", tstatnum)
+		err := driver.AddActuationCallback("virtual_thermostat", instance, func(msg *xbospb.XBOSIoTDeviceActuation, received time.Time) error {
+			tstat := msg.Thermostat
+			fmt.Println("actuation?", tstat, received)
+			if tstat.HeatingSetpoint != nil {
+				driver.hsp = tstat.HeatingSetpoint.Value
 			}
-		}()
+			if tstat.CoolingSetpoint != nil {
+				driver.csp = tstat.CoolingSetpoint.Value
+			}
+			reading := &xbospb.XBOSIoTDeviceState{
+				Thermostat: &xbospb.Thermostat{
+					Temperature:     &xbospb.Double{Value: driver.temp},
+					HeatingSetpoint: &xbospb.Double{Value: driver.hsp},
+					CoolingSetpoint: &xbospb.Double{Value: driver.csp},
+					State:           &xbospb.HVACState{Value: xbospb.HVACStateValue(driver.state)},
+				},
+			}
+			return driver.Respond("virtual_thermostat", instance, uint64(msg.Requestid), reading)
+		})
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		err = driver.AddReport("virtual_thermostat", instance, func() (*xbospb.XBOSIoTDeviceState, error) {
+			// update thermostat state
+			if driver.state == 2 {
+				driver.temp -= rand.Float64()
+			} else if driver.state == 1 {
+				driver.temp += rand.Float64()
+			} else {
+				driver.temp += 0.5 - rand.Float64()
+			}
+
+			if driver.temp <= driver.hsp-1 {
+				driver.state = 1
+			} else if driver.temp >= driver.csp+1 {
+				driver.state = 2
+			} else {
+				driver.state = 0
+			}
+
+			reading := &xbospb.XBOSIoTDeviceState{
+				Thermostat: &xbospb.Thermostat{
+					Temperature:     &xbospb.Double{Value: driver.temp},
+					HeatingSetpoint: &xbospb.Double{Value: driver.hsp},
+					CoolingSetpoint: &xbospb.Double{Value: driver.csp},
+					State:           &xbospb.HVACState{Value: xbospb.HVACStateValue(driver.state)},
+				},
+			}
+			return reading, nil
+		})
+		if err != nil {
+			log.Fatal(err)
+		}
 	}
 }
 
 func main() {
 	cfg := driver.Config{
-		Namespace:  "GyAqNmq2Clb8Z17CAIJK95EPecGHb8MSK0CpWtYCLTP1JA==",
+		Namespace:  "GyCetklhSNcgsCKVKXxSuCUZP4M80z9NRxU1pwfb2XwGhg==",
 		EntityFile: "driver.ent",
 		SiteRouter: "localhost:4516",
+		ReportRate: 10 * time.Second,
 	}
 	driver := newVirtualThermostatDriver(1, cfg)
 
