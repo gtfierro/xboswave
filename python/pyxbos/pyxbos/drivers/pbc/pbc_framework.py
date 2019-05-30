6fc9c3bbc015ab6f53159bb59b457d4b45d5c5a0
Gabe Fierro <gtfierro225@gmail.com>
2019-05-29 17:24:34 -0700

    add pyxbos process class, pbc framework for energise

---
+++ python/pyxbos/pyxbos/drivers/pbc/pbc_framework.py
@@ -0,0 +1,89 @@
+from pyxbos.process import XBOSProcess, b64decode, b64encode, schedule, run_loop
+from pyxbos.xbos_pb2 import XBOS
+from pyxbos.energise_pb2 import EnergiseMessage, LPBCStatus, LPBCCommand, SPBC, EnergiseError
+from pyxbos.c37_pb2 import Phasor
+from datetime import datetime
+from collections import deque
+
+class SPBCProcess(XBOSProcess):
+    def __init__(self, cfg):
+        super().__init__(cfg)
+        if 'namespace' not in cfg:
+            raise ConfigMissingError('namespace')
+        self.namespace = b64decode(cfg['namespace'])
+        self._log.info(f"initialized SPBC: {cfg}")
+
+    async def broadcast_target(self, p, q):
+        self._log.info(f"SPBC announcing p {p} q {q}")
+        await self.publish(self.namespace, "spbc/1", XBOS(
+            EnergiseMessage = EnergiseMessage(
+            SPBC=SPBC(
+                time=int(datetime.utcnow().timestamp()*1e9),
+                phasor_target=Phasor(P=p, Q=q)
+                )
+            )))
+
+
+class LPBCProcess(XBOSProcess):
+    def __init__(self, cfg):
+        super().__init__(cfg)
+        if 'namespace' not in cfg:
+            raise ConfigMissingError('namespace')
+        self.namespace = b64decode(cfg['namespace'])
+        if 'upmu' not in cfg:
+            raise ConfigMissingError('upmu')
+        if 'name' not in cfg:
+            raise ConfigMissingError('name')
+        self.upmu = cfg['upmu']
+        self.name = cfg['name']
+        self.last_upmu_reading = None
+        self.last_spbc_command = None
+        self.control_on = False
+
+        schedule(self.subscribe_extract(self.namespace, f"upmu/{self.upmu}", ".C37DataFrame.phasorChannels[].data[]", self._upmucb))
+        schedule(self.subscribe_extract(self.namespace, "spbc/1", ".EnergiseMessage.SPBC", self._spbccb))
+	
+        schedule(self.call_periodic(2, self._trigger, runfirst=False))
+
+        self._log.info(f"initialized LPBC: {cfg}")
+
+    def _upmucb(self, resp):
+        self.last_upmu_reading = resp
+    def _spbccb(self, resp):
+        self.last_spbc_command = resp
+
+    async def _trigger(self):
+        try:
+            if self.last_upmu_reading is None or len(self.last_upmu_reading.values) == 0:
+                self._log.warning(f"LPBC {self.name} has not received a upmu reading")
+                return
+            if self.last_spbc_command is None or len(self.last_spbc_command.values) == 0:
+                self._log.warning(f"LPBC {self.name} has not received an SPBC command")
+                return
+            upmu_value = self.last_upmu_reading.values[-1] # most recent
+            timestamp = datetime.utcfromtimestamp(int(upmu_value['time']) / 1e9)
+            spbc_cmd = self.last_spbc_command.values[-1] # most recent
+            targets = spbc_cmd['phasorTarget']
+            await self.do_trigger(timestamp, upmu_value['magnitude'], upmu_value['angle'], targets['P'], targets['Q'])
+        except IndexError:
+            return # no upmu readings
+
+    async def do_trigger(self, timestamp, vmag, vang, p_target, q_target):
+        self._log.info(f"""LPBC {self.name} received call at {timestamp}:
+    V(mag): {vmag}
+    V(ang): {vang}
+    SPBC targets: P: {p_target} Q: {q_target}
+    Enable control?: {self.control_on}
+        """)
+        error, mp, mq, sat = self.step(timestamp, vmag, vang, p_target, q_target)
+
+        msg = XBOS(
+                EnergiseMessage=EnergiseMessage(
+                    LPBCStatus=LPBCStatus(
+                        time=int(datetime.utcnow().timestamp()*1e9),
+                        phasors=Phasor(P=mp, Q=mq),
+                        saturated=sat,
+                        do_control=self.control_on,
+                    )
+                ))
+        await self.publish(self.namespace, f"lpbc/{self.name}", msg)
