f7b79bb4219e647a97c3419cf0cb8832385247a8
john-b-yang <byjohnyang@gmail.com>
2019-06-04 14:46:31 -0700

    Initial Commit

---
+++ python/pyxbos/pyxbos/drivers/obvius/obvius.py
@@ -0,0 +1,7 @@
+from pyxbos.driver import *
+import logging
+import os
+import time
+
+if __name__ == '__main__':
+    logging.basicConfig(level="INFO", format='%(asctime)s - %(name)s - %(message)s')
