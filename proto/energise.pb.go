af112a401c79bc698c3c2b5df7557a016f68091f
Gabe Fierro <gtfierro@cs.berkeley.edu>
2019-07-01 19:57:57 -0700

    updating energise proto files to include new data [backwards incompatible]

--- proto/energise.pb.go
+++ proto/energise.pb.go
@@ -21,9 +21,9 @@ var _ = math.Inf
 const _ = proto.ProtoPackageIsVersion3 // please upgrade the proto package
 
 type EnergiseMessage struct {
-	SPBC                 *SPBC        `protobuf:"bytes,1,opt,name=SPBC,proto3" json:"SPBC,omitempty"`
-	LPBCStatus           *LPBCStatus  `protobuf:"bytes,2,opt,name=LPBCStatus,proto3" json:"LPBCStatus,omitempty"`
-	LPBCCommand          *LPBCCommand `protobuf:"bytes,3,opt,name=LPBCCommand,proto3" json:"LPBCCommand,omitempty"`
+	SPBC                 *SPBC        `protobuf:"bytes,1,opt,name=SPBC,json=sPBC,proto3" json:"SPBC,omitempty"`
+	LPBCStatus           *LPBCStatus  `protobuf:"bytes,2,opt,name=LPBCStatus,json=lPBCStatus,proto3" json:"LPBCStatus,omitempty"`
+	LPBCCommand          *LPBCCommand `protobuf:"bytes,3,opt,name=LPBCCommand,json=lPBCCommand,proto3" json:"LPBCCommand,omitempty"`
 	XXX_NoUnkeyedLiteral struct{}     `json:"-"`
 	XXX_unrecognized     []byte       `json:"-"`
 	XXX_sizecache        int32        `json:"-"`
@@ -114,10 +114,83 @@ func (m *EnergiseError) GetMsg() string {
 	return ""
 }
 
+// EnergisePhasorTarget is a control target specified on a per-node basis
+// upmu0 is the 'head of feeder'. This is reference for everything: SPBC, LPBCs
+type EnergisePhasorTarget struct {
+	NodeID               string   `protobuf:"bytes,1,opt,name=nodeID,proto3" json:"nodeID,omitempty"`
+	Magnitude            float64  `protobuf:"fixed64,2,opt,name=magnitude,proto3" json:"magnitude,omitempty"`
+	Angle                float64  `protobuf:"fixed64,3,opt,name=angle,proto3" json:"angle,omitempty"`
+	Kvbase               float64  `protobuf:"fixed64,4,opt,name=kvbase,proto3" json:"kvbase,omitempty"`
+	XXX_NoUnkeyedLiteral struct{} `json:"-"`
+	XXX_unrecognized     []byte   `json:"-"`
+	XXX_sizecache        int32    `json:"-"`
+}
+
+func (m *EnergisePhasorTarget) Reset()         { *m = EnergisePhasorTarget{} }
+func (m *EnergisePhasorTarget) String() string { return proto.CompactTextString(m) }
+func (*EnergisePhasorTarget) ProtoMessage()    {}
+func (*EnergisePhasorTarget) Descriptor() ([]byte, []int) {
+	return fileDescriptor_a37b97c5e2bc6c22, []int{2}
+}
+
+func (m *EnergisePhasorTarget) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_EnergisePhasorTarget.Unmarshal(m, b)
+}
+func (m *EnergisePhasorTarget) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_EnergisePhasorTarget.Marshal(b, m, deterministic)
+}
+func (m *EnergisePhasorTarget) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_EnergisePhasorTarget.Merge(m, src)
+}
+func (m *EnergisePhasorTarget) XXX_Size() int {
+	return xxx_messageInfo_EnergisePhasorTarget.Size(m)
+}
+func (m *EnergisePhasorTarget) XXX_DiscardUnknown() {
+	xxx_messageInfo_EnergisePhasorTarget.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_EnergisePhasorTarget proto.InternalMessageInfo
+
+func (m *EnergisePhasorTarget) GetNodeID() string {
+	if m != nil {
+		return m.NodeID
+	}
+	return ""
+}
+
+func (m *EnergisePhasorTarget) GetMagnitude() float64 {
+	if m != nil {
+		return m.Magnitude
+	}
+	return 0
+}
+
+func (m *EnergisePhasorTarget) GetAngle() float64 {
+	if m != nil {
+		return m.Angle
+	}
+	return 0
+}
+
+func (m *EnergisePhasorTarget) GetKvbase() float64 {
+	if m != nil {
+		return m.Kvbase
+	}
+	return 0
+}
+
+// The SPBC message is sent by a supervisory controller (also called an SPBC)
+// at regular intervals.  The expectation is the SPBC will send out a single
+// message for each node that it is controlling, containing an
+// EnergisePhasorTarget for that node. We restrict each message to a single
+// phasor_target for now in order to maintain isolation between the nodes and
+// bound what information they are allowed to see.
 type SPBC struct {
 	// current time of announcement in milliseconds
-	Time                 int64          `protobuf:"varint,1,opt,name=time,proto3" json:"time,omitempty"`
-	PhasorTarget         *Phasor        `protobuf:"bytes,2,opt,name=phasor_target,json=phasorTarget,proto3" json:"phasor_target,omitempty"`
+	Time int64 `protobuf:"varint,1,opt,name=time,proto3" json:"time,omitempty"`
+	// a phasor target for a specific node
+	PhasorTarget *EnergisePhasorTarget `protobuf:"bytes,2,opt,name=phasor_target,json=phasorTarget,proto3" json:"phasor_target,omitempty"`
+	// represents general errors in the SPBC
 	Error                *EnergiseError `protobuf:"bytes,3,opt,name=error,proto3" json:"error,omitempty"`
 	XXX_NoUnkeyedLiteral struct{}       `json:"-"`
 	XXX_unrecognized     []byte         `json:"-"`
@@ -128,7 +201,7 @@ func (m *SPBC) Reset()         { *m = SPBC{} }
 func (m *SPBC) String() string { return proto.CompactTextString(m) }
 func (*SPBC) ProtoMessage()    {}
 func (*SPBC) Descriptor() ([]byte, []int) {
-	return fileDescriptor_a37b97c5e2bc6c22, []int{2}
+	return fileDescriptor_a37b97c5e2bc6c22, []int{3}
 }
 
 func (m *SPBC) XXX_Unmarshal(b []byte) error {
@@ -156,7 +229,7 @@ func (m *SPBC) GetTime() int64 {
 	return 0
 }
 
-func (m *SPBC) GetPhasorTarget() *Phasor {
+func (m *SPBC) GetPhasorTarget() *EnergisePhasorTarget {
 	if m != nil {
 		return m.PhasorTarget
 	}
@@ -175,22 +248,26 @@ type LPBCStatus struct {
 	Time int64 `protobuf:"varint,1,opt,name=time,proto3" json:"time,omitempty"`
 	// contains potential errors
 	Error *EnergiseError `protobuf:"bytes,2,opt,name=error,proto3" json:"error,omitempty"`
-	// current P, Q of LPBC
-	Phasors *Phasor `protobuf:"bytes,3,opt,name=phasors,proto3" json:"phasors,omitempty"`
-	// true if LPBC is saturated
-	Saturated bool `protobuf:"varint,4,opt,name=saturated,proto3" json:"saturated,omitempty"`
-	// true of the LPBC is performing control
-	DoControl            bool     `protobuf:"varint,5,opt,name=do_control,json=doControl,proto3" json:"do_control,omitempty"`
-	XXX_NoUnkeyedLiteral struct{} `json:"-"`
-	XXX_unrecognized     []byte   `json:"-"`
-	XXX_sizecache        int32    `json:"-"`
+	// current P, Q errors of LPBC
+	PhasorErrors *Phasor `protobuf:"bytes,3,opt,name=phasor_errors,json=phasorErrors,proto3" json:"phasor_errors,omitempty"`
+	// true if LPBC P is saturated
+	PSaturated bool `protobuf:"varint,4,opt,name=p_saturated,json=pSaturated,proto3" json:"p_saturated,omitempty"`
+	// true if LPBC Q is saturated
+	QSaturated bool `protobuf:"varint,5,opt,name=q_saturated,json=qSaturated,proto3" json:"q_saturated,omitempty"`
+	// true if the LPBC is performing control
+	DoControl bool `protobuf:"varint,6,opt,name=do_control,json=doControl,proto3" json:"do_control,omitempty"`
+	// should be populated if p_saturated or q_saturated
+	Pqmax                *LPBCStatus_Max `protobuf:"bytes,7,opt,name=pqmax,proto3" json:"pqmax,omitempty"`
+	XXX_NoUnkeyedLiteral struct{}        `json:"-"`
+	XXX_unrecognized     []byte          `json:"-"`
+	XXX_sizecache        int32           `json:"-"`
 }
 
 func (m *LPBCStatus) Reset()         { *m = LPBCStatus{} }
 func (m *LPBCStatus) String() string { return proto.CompactTextString(m) }
 func (*LPBCStatus) ProtoMessage()    {}
 func (*LPBCStatus) Descriptor() ([]byte, []int) {
-	return fileDescriptor_a37b97c5e2bc6c22, []int{3}
+	return fileDescriptor_a37b97c5e2bc6c22, []int{4}
 }
 
 func (m *LPBCStatus) XXX_Unmarshal(b []byte) error {
@@ -225,16 +302,23 @@ func (m *LPBCStatus) GetError() *EnergiseError {
 	return nil
 }
 
-func (m *LPBCStatus) GetPhasors() *Phasor {
+func (m *LPBCStatus) GetPhasorErrors() *Phasor {
 	if m != nil {
-		return m.Phasors
+		return m.PhasorErrors
 	}
 	return nil
 }
 
-func (m *LPBCStatus) GetSaturated() bool {
+func (m *LPBCStatus) GetPSaturated() bool {
 	if m != nil {
-		return m.Saturated
+		return m.PSaturated
+	}
+	return false
+}
+
+func (m *LPBCStatus) GetQSaturated() bool {
+	if m != nil {
+		return m.QSaturated
 	}
 	return false
 }
@@ -244,6 +328,60 @@ func (m *LPBCStatus) GetDoControl() bool {
 		return m.DoControl
 	}
 	return false
+}
+
+func (m *LPBCStatus) GetPqmax() *LPBCStatus_Max {
+	if m != nil {
+		return m.Pqmax
+	}
+	return nil
+}
+
+type LPBCStatus_Max struct {
+	Pmax                 float64  `protobuf:"fixed64,1,opt,name=Pmax,json=pmax,proto3" json:"Pmax,omitempty"`
+	Pmin                 float64  `protobuf:"fixed64,2,opt,name=Pmin,json=pmin,proto3" json:"Pmin,omitempty"`
+	XXX_NoUnkeyedLiteral struct{} `json:"-"`
+	XXX_unrecognized     []byte   `json:"-"`
+	XXX_sizecache        int32    `json:"-"`
+}
+
+func (m *LPBCStatus_Max) Reset()         { *m = LPBCStatus_Max{} }
+func (m *LPBCStatus_Max) String() string { return proto.CompactTextString(m) }
+func (*LPBCStatus_Max) ProtoMessage()    {}
+func (*LPBCStatus_Max) Descriptor() ([]byte, []int) {
+	return fileDescriptor_a37b97c5e2bc6c22, []int{4, 0}
+}
+
+func (m *LPBCStatus_Max) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_LPBCStatus_Max.Unmarshal(m, b)
+}
+func (m *LPBCStatus_Max) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_LPBCStatus_Max.Marshal(b, m, deterministic)
+}
+func (m *LPBCStatus_Max) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_LPBCStatus_Max.Merge(m, src)
+}
+func (m *LPBCStatus_Max) XXX_Size() int {
+	return xxx_messageInfo_LPBCStatus_Max.Size(m)
+}
+func (m *LPBCStatus_Max) XXX_DiscardUnknown() {
+	xxx_messageInfo_LPBCStatus_Max.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_LPBCStatus_Max proto.InternalMessageInfo
+
+func (m *LPBCStatus_Max) GetPmax() float64 {
+	if m != nil {
+		return m.Pmax
+	}
+	return 0
+}
+
+func (m *LPBCStatus_Max) GetPmin() float64 {
+	if m != nil {
+		return m.Pmin
+	}
+	return 0
 }
 
 type LPBCCommand struct {
@@ -263,7 +401,7 @@ func (m *LPBCCommand) Reset()         { *m = LPBCCommand{} }
 func (m *LPBCCommand) String() string { return proto.CompactTextString(m) }
 func (*LPBCCommand) ProtoMessage()    {}
 func (*LPBCCommand) Descriptor() ([]byte, []int) {
-	return fileDescriptor_a37b97c5e2bc6c22, []int{4}
+	return fileDescriptor_a37b97c5e2bc6c22, []int{5}
 }
 
 func (m *LPBCCommand) XXX_Unmarshal(b []byte) error {
@@ -308,33 +446,44 @@ func (m *LPBCCommand) GetDoControl() bool {
 func init() {
 	proto.RegisterType((*EnergiseMessage)(nil), "xbospb.EnergiseMessage")
 	proto.RegisterType((*EnergiseError)(nil), "xbospb.EnergiseError")
+	proto.RegisterType((*EnergisePhasorTarget)(nil), "xbospb.EnergisePhasorTarget")
 	proto.RegisterType((*SPBC)(nil), "xbospb.SPBC")
 	proto.RegisterType((*LPBCStatus)(nil), "xbospb.LPBCStatus")
+	proto.RegisterType((*LPBCStatus_Max)(nil), "xbospb.LPBCStatus.Max")
 	proto.RegisterType((*LPBCCommand)(nil), "xbospb.LPBCCommand")
 }
 
 func init() { proto.RegisterFile("energise.proto", fileDescriptor_a37b97c5e2bc6c22) }
 
 var fileDescriptor_a37b97c5e2bc6c22 = []byte{
-	// 317 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xac, 0x92, 0xcd, 0x4a, 0xf3, 0x40,
-	0x14, 0x86, 0x49, 0xd3, 0xf6, 0xfb, 0x72, 0xfa, 0xa3, 0x1c, 0x11, 0x82, 0x28, 0xc4, 0xac, 0x02,
-	0x42, 0x16, 0x0d, 0xe2, 0xbe, 0xa1, 0x3b, 0x85, 0x30, 0x75, 0x5f, 0x26, 0xcd, 0x10, 0x0b, 0x26,
-	0x13, 0x66, 0x26, 0xe0, 0xc2, 0x6b, 0xf1, 0x2a, 0xbc, 0x40, 0xc9, 0x4c, 0x62, 0x93, 0x22, 0xb8,
-	0x71, 0x37, 0x3d, 0xef, 0x73, 0xe6, 0x7d, 0x98, 0x06, 0x96, 0xac, 0x64, 0x22, 0x3f, 0x48, 0x16,
-	0x56, 0x82, 0x2b, 0x8e, 0xd3, 0xb7, 0x94, 0xcb, 0x2a, 0xbd, 0x72, 0xf6, 0xd1, 0x83, 0x19, 0xf9,
-	0x1f, 0x16, 0x9c, 0x6d, 0x5a, 0xea, 0x89, 0x49, 0x49, 0x73, 0x86, 0x1e, 0x8c, 0xb7, 0xc9, 0x3a,
-	0x76, 0x2d, 0xcf, 0x0a, 0x66, 0xab, 0x79, 0x68, 0xb6, 0xc2, 0x66, 0x46, 0x74, 0x82, 0x2b, 0x80,
-	0xc7, 0x64, 0x1d, 0x6f, 0x15, 0x55, 0xb5, 0x74, 0x47, 0x9a, 0xc3, 0x8e, 0x3b, 0x26, 0xa4, 0x47,
-	0xe1, 0x3d, 0xcc, 0x9a, 0x5f, 0x31, 0x2f, 0x0a, 0x5a, 0x66, 0xae, 0xad, 0x97, 0x2e, 0xfa, 0x4b,
-	0x6d, 0x44, 0xfa, 0x9c, 0x7f, 0x0b, 0x8b, 0xce, 0x6f, 0x23, 0x04, 0x17, 0x78, 0x0e, 0x76, 0x21,
-	0x73, 0x2d, 0xe7, 0x90, 0xe6, 0xe8, 0xbf, 0x1b, 0x5f, 0x44, 0x18, 0xab, 0x43, 0xc1, 0x74, 0x64,
-	0x13, 0x7d, 0xc6, 0x08, 0x16, 0xd5, 0x0b, 0x95, 0x5c, 0xec, 0x14, 0x15, 0x39, 0x53, 0xad, 0xec,
-	0xb2, 0xeb, 0x4d, 0x74, 0x48, 0xe6, 0x06, 0x7a, 0xd6, 0x0c, 0xde, 0xc1, 0x84, 0x35, 0x5d, 0xad,
-	0xe4, 0x65, 0x07, 0x0f, 0x44, 0x88, 0x61, 0xfc, 0x4f, 0xab, 0xff, 0x18, 0x3f, 0x4a, 0x7c, 0xdf,
-	0x37, 0xfa, 0xfd, 0x3e, 0x0c, 0xe0, 0x9f, 0x91, 0x91, 0x6d, 0xfd, 0xa9, 0x6b, 0x17, 0xe3, 0x35,
-	0x38, 0x92, 0xaa, 0x5a, 0x50, 0xc5, 0x32, 0x77, 0xec, 0x59, 0xc1, 0x7f, 0x72, 0x1c, 0xe0, 0x0d,
-	0x40, 0xc6, 0x77, 0x7b, 0x5e, 0x2a, 0xc1, 0x5f, 0xdd, 0x89, 0x89, 0x33, 0x1e, 0x9b, 0x81, 0x5f,
-	0x0f, 0xfe, 0x8e, 0xbf, 0x7b, 0xbb, 0x61, 0xad, 0x7d, 0x52, 0x9b, 0x4e, 0xf5, 0x67, 0x17, 0x7d,
-	0x05, 0x00, 0x00, 0xff, 0xff, 0x6f, 0x4c, 0x45, 0x5e, 0x9b, 0x02, 0x00, 0x00,
+	// 455 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x84, 0x53, 0xd1, 0x6e, 0xd3, 0x30,
+	0x14, 0x55, 0x9a, 0xb4, 0xd0, 0xdb, 0x6d, 0x20, 0x33, 0xa6, 0x68, 0x1a, 0x62, 0xe4, 0x09, 0x09,
+	0xe8, 0xc3, 0x2a, 0xc4, 0x33, 0x0d, 0x7b, 0x40, 0xa2, 0x52, 0xe4, 0xf2, 0x5e, 0xb9, 0x8b, 0x15,
+	0x22, 0x9a, 0xd8, 0xb5, 0x5d, 0x54, 0xf1, 0x09, 0x7c, 0x04, 0xdf, 0xc2, 0xa7, 0x21, 0x5f, 0xdb,
+	0x6d, 0x57, 0x82, 0xf6, 0xe6, 0x7b, 0xee, 0x39, 0xce, 0xb9, 0xe7, 0x3a, 0x70, 0xc6, 0x5b, 0xae,
+	0xaa, 0x5a, 0xf3, 0xb1, 0x54, 0xc2, 0x08, 0x32, 0xd8, 0x2e, 0x85, 0x96, 0xcb, 0xcb, 0xe1, 0xdd,
+	0xe4, 0x83, 0x83, 0xb2, 0xdf, 0x11, 0x3c, 0xb9, 0xf5, 0xac, 0x19, 0xd7, 0x9a, 0x55, 0x9c, 0x5c,
+	0x43, 0x32, 0x2f, 0xa6, 0x79, 0x1a, 0x5d, 0x47, 0xaf, 0x47, 0x37, 0x27, 0x63, 0xa7, 0x1a, 0x5b,
+	0x8c, 0x26, 0xba, 0x98, 0xe6, 0xe4, 0x06, 0xe0, 0x4b, 0x31, 0xcd, 0xe7, 0x86, 0x99, 0x8d, 0x4e,
+	0x7b, 0xc8, 0x23, 0x81, 0xb7, 0xef, 0x50, 0x58, 0xed, 0xce, 0xe4, 0x3d, 0x8c, 0x6c, 0x27, 0x17,
+	0x4d, 0xc3, 0xda, 0x32, 0x8d, 0x51, 0xf4, 0xec, 0x50, 0xe4, 0x5b, 0x74, 0xb4, 0xda, 0x17, 0xd9,
+	0x2b, 0x38, 0x0d, 0xfe, 0x6e, 0x95, 0x12, 0x8a, 0x3c, 0x85, 0xb8, 0xd1, 0x15, 0x9a, 0x1b, 0x52,
+	0x7b, 0xcc, 0x7e, 0xc2, 0x79, 0xa0, 0x14, 0xdf, 0x98, 0x16, 0xea, 0x2b, 0x53, 0x15, 0x37, 0xe4,
+	0x02, 0x06, 0xad, 0x28, 0xf9, 0xe7, 0x4f, 0x9e, 0xec, 0x2b, 0x72, 0x05, 0xc3, 0x86, 0x55, 0x6d,
+	0x6d, 0x36, 0x25, 0x47, 0xf3, 0x11, 0xdd, 0x03, 0xe4, 0x1c, 0xfa, 0xac, 0xad, 0x56, 0x1c, 0x1d,
+	0x46, 0xd4, 0x15, 0xf6, 0xae, 0xef, 0x3f, 0x96, 0x4c, 0xf3, 0x34, 0x41, 0xd8, 0x57, 0xd9, 0xaf,
+	0xc8, 0x85, 0x45, 0x08, 0x24, 0xa6, 0x6e, 0x38, 0x7e, 0x2a, 0xa6, 0x78, 0x26, 0x1f, 0xe1, 0x54,
+	0xa2, 0xa1, 0x85, 0x41, 0x47, 0x3e, 0xa9, 0xab, 0x30, 0x74, 0x97, 0x6b, 0x7a, 0x22, 0x0f, 0x67,
+	0x78, 0x03, 0x7d, 0x6e, 0xc7, 0xf6, 0x79, 0x3d, 0x3f, 0x96, 0x62, 0x26, 0xd4, 0x71, 0xb2, 0x3f,
+	0xbd, 0xc3, 0xbd, 0x74, 0x5a, 0xda, 0xdd, 0xd7, 0x7b, 0xf8, 0x3e, 0x32, 0xd9, 0xf9, 0xc7, 0x5a,
+	0x7b, 0x13, 0x67, 0x41, 0xe4, 0x7c, 0x07, 0xc7, 0x28, 0xd5, 0xe4, 0x25, 0x8c, 0xe4, 0x42, 0x33,
+	0xb3, 0x51, 0xcc, 0xf0, 0x12, 0xe3, 0x7a, 0x4c, 0x41, 0xce, 0x03, 0x62, 0x09, 0xeb, 0x03, 0x42,
+	0xdf, 0x11, 0xd6, 0x7b, 0xc2, 0x0b, 0x80, 0x52, 0x2c, 0xee, 0x44, 0x6b, 0x94, 0x58, 0xa5, 0x03,
+	0xec, 0x0f, 0x4b, 0x91, 0x3b, 0x80, 0xbc, 0x85, 0xbe, 0x5c, 0x37, 0x6c, 0x9b, 0x3e, 0x42, 0x37,
+	0x17, 0xff, 0xbe, 0xbb, 0xf1, 0x8c, 0x6d, 0xa9, 0x23, 0x5d, 0xbe, 0x83, 0x78, 0xc6, 0xb6, 0x36,
+	0x8b, 0xc2, 0x6a, 0x22, 0xdc, 0x5e, 0x22, 0x9b, 0x80, 0xd5, 0xad, 0x7f, 0x02, 0x89, 0x6c, 0xea,
+	0x36, 0xdb, 0xdc, 0x7b, 0xa5, 0x9d, 0x11, 0x4e, 0xba, 0xb7, 0xfa, 0x9f, 0x54, 0xfc, 0x1e, 0xef,
+	0xcf, 0x14, 0x1f, 0xcd, 0xb4, 0x1c, 0xe0, 0xdf, 0x38, 0xf9, 0x1b, 0x00, 0x00, 0xff, 0xff, 0xa0,
+	0x19, 0x60, 0x94, 0xb2, 0x03, 0x00, 0x00,
 }
