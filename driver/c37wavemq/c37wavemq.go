3b2b15e3494c485070551a0da5eb0c113f1dc313
Gabe Fierro <gtfierro@cs.berkeley.edu>
2019-07-03 10:25:11 -0700

    Energise c37 rework (#32)
    * updating energise proto files to include new data [backwards incompatible]
    
    * regenerating _pb2 files; working on pbc framework
    
    * use existing nullabletypes
    
    * spbc framework progress
    
    * lpbc framework progress
    
    * fix spbc operation
    
    * fixing phasor handling
    
    * updating energise protobuf, lowering batch size for c37
    
    * remove nonfun config
    
    * switch to 30-reading frames
    
    * update pbc framework

--- driver/c37wavemq/c37wavemq.go
+++ driver/c37wavemq/c37wavemq.go
@@ -70,7 +70,8 @@ func StartProtocolAdapter(cfg *ProtocolAdapterConfig) {
 	}
 }
 
-const BatchSize = 120
+//const BatchSize = 120
+const BatchSize = 30
 const Proportion = 1 // 100% of readings
 var _batchstep = 1   //int(Proportion * BatchSize)
 
