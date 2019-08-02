018418c31c1c417734b707953d990adec286a104
Gabe Fierro <gtfierro225@gmail.com>
2019-08-02 12:15:56 -0400

    add lpbc archival

--- ingester/plugins/energise.go
+++ ingester/plugins/energise.go
@@ -42,7 +42,6 @@ func Extract(uri types.SubscriptionURI, msg xbospb.XBOS, add func(types.Extracte
 					return err
 				}
 			}
-			fmt.Printf("%+v\n", extracted)
 
 			// archive magnitude
 			extracted.Tags["name"] = "magnitude"
@@ -55,7 +54,105 @@ func Extract(uri types.SubscriptionURI, msg xbospb.XBOS, add func(types.Extracte
 					return err
 				}
 			}
-			fmt.Printf("%+v\n", extracted)
+		}
+	}
+
+	// archive LPBC Status
+	if msg.EnergiseMessage.LPBCStatus != nil {
+		lpbc := msg.EnergiseMessage.LPBCStatus
+		timestamp := lpbc.Time
+
+		var extracted types.ExtractedTimeseries
+		extracted.Tags = make(map[string]string)
+		for _, channel_status := range lpbc.Statuses {
+			extracted.Tags["nodeID"] = channel_status.NodeID
+			extracted.Tags["channel_name"] = channel_status.ChannelName
+			extracted.Collection = fmt.Sprintf("xbos/%s/%s", uri.Resource, channel_status.ChannelName)
+
+			// archive phasor_errors
+			// V == magnitude
+			extracted.Tags["name"] = "V"
+			extracted.Values = []float64{channel_status.PhasorErrors.Magnitude}
+			extracted.Times = []int64{timestamp}
+			extracted.UUID = types.GenerateUUID(uri, []byte(extracted.Collection+"V"))
+			if !extracted.Empty() {
+				if err := add(extracted); err != nil {
+					fmt.Println(err)
+					return err
+				}
+			}
+
+			// delta == Angle
+			extracted.Tags["name"] = "delta"
+			extracted.Values = []float64{channel_status.PhasorErrors.Angle}
+			extracted.Times = []int64{timestamp}
+			extracted.UUID = types.GenerateUUID(uri, []byte(extracted.Collection+"delta"))
+			if !extracted.Empty() {
+				if err := add(extracted); err != nil {
+					fmt.Println(err)
+					return err
+				}
+			}
+
+			// archive p_saturated
+			extracted.Tags["name"] = "p_saturated"
+			var pSatVal = 0
+			if channel_status.PSaturated {
+				pSatVal = 1
+			}
+			extracted.Values = []float64{float64(pSatVal)}
+			extracted.Times = []int64{timestamp}
+			extracted.UUID = types.GenerateUUID(uri, []byte(extracted.Collection+"p_saturated"))
+			if !extracted.Empty() {
+				if err := add(extracted); err != nil {
+					fmt.Println(err)
+					return err
+				}
+			}
+
+			// archive q_saturated
+			extracted.Tags["name"] = "q_saturated"
+			var qSatVal = 0
+			if channel_status.QSaturated {
+				qSatVal = 1
+			}
+			extracted.Values = []float64{float64(qSatVal)}
+			extracted.Times = []int64{timestamp}
+			extracted.UUID = types.GenerateUUID(uri, []byte(extracted.Collection+"q_saturated"))
+			if !extracted.Empty() {
+				if err := add(extracted); err != nil {
+					fmt.Println(err)
+					return err
+				}
+			}
+
+			// archive p_max
+			if channel_status.PMax != nil {
+				extracted.Tags["name"] = "p_max"
+				extracted.Values = []float64{channel_status.PMax.Value}
+				extracted.Times = []int64{timestamp}
+				extracted.UUID = types.GenerateUUID(uri, []byte(extracted.Collection+"p_max"))
+				if !extracted.Empty() {
+					if err := add(extracted); err != nil {
+						fmt.Println(err)
+						return err
+					}
+				}
+			}
+
+			// archive q_max
+			if channel_status.QMax != nil {
+				extracted.Tags["name"] = "q_max"
+				extracted.Values = []float64{channel_status.QMax.Value}
+				extracted.Times = []int64{timestamp}
+				extracted.UUID = types.GenerateUUID(uri, []byte(extracted.Collection+"q_max"))
+				if !extracted.Empty() {
+					if err := add(extracted); err != nil {
+						fmt.Println(err)
+						return err
+					}
+				}
+			}
 		}
 
 	}
