1e321aab3dd6629c4875e3f1c58d06ac025581c3
Gabe Fierro <gtfierro@cs.berkeley.edu>
2019-09-09 22:00:59 -0700

    add another lib

--- setup/run.sh
+++ setup/run.sh
@@ -3,7 +3,7 @@
 source env.sh
 
 # install deps
-sudo apt install libsnappy-dev zlib1g-dev libbz2-dev liblz4-dev libzstd-dev
+sudo apt install libsnappy-dev zlib1g-dev libbz2-dev liblz4-dev libzstd-dev libgflags-dev
 
 . download_binaries.sh
 . setup_config.sh
